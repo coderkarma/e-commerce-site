462fe4558a8e92593cef4e5122c25b73b9d38de2
Karma Drukpa <Karma_Drukpa@epam.com>
2019-11-18 23:25:02 -0800

    created create react app and set up file structure with some components

---
+++ src/components/Directory/Directory.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Directory extends Component {
+    state = {
+        sections: []
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
