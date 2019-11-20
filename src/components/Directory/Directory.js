0835cfec4fff0a5f43e9bdf3d199e80201c8c118
Karma Drukpa <Karma_Drukpa@epam.com>
2019-11-19 16:47:06 -0800

    added hardcoded image urls and separated the functionality

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -1,14 +1,52 @@
-import React, { Component } from 'react'
-
-export default class Directory extends Component {
-    state = {
-        sections: []
-    }
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+import React, { Component } from 'react';
+import MenuItem from '../MenuItem/MenuItem';
+import './Directory.scss';
+class Directory extends Component {
+  state = {
+    sections: [
+      {
+        title: 'hats',
+        imageUrl: 'https://www.dhresource.com/0x0s/f2-albu-g6-M01-1E-1B-rBVaSFuqXumAWKmjAAFZZK31xFI315.jpg/new-luxury-brand-men-women-loafers-sneakers.jpg',
+        id: 1,
+        linkUrl: 'shop/hats'
+      },
+      {
+        title: 'jackets',
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtDAKrI7fvzNIr6z8ZDIZJyCSgOI0X7xeW_ctI5MR3j8IGE9ei&s',
+        id: 2,
+        linkUrl: 'shop/jackets'
+      },
+      {
+        title: 'sneakers',
+        imageUrl: 'https://my-test-11.slatic.net/original/e875c5ce1b9dcd0b630d4dcc99ed9cee.jpg',
+        id: 3,
+        linkUrl: 'shop/sneakers'
+      },
+      {
+        title: 'womens',
+        imageUrl: 'https://gd.image-gmkt.com/WOMEN-FASHION-SOLID-HIGH-WAIST-WIDE-LEG-PANTS-STRAP-PALAZZO-LOUNGE/li/069/405/859405069.g_400-w_g.jpg',
+        id: 4,
+        linkUrl: 'shop/womens',
+        size: 'large'
+      },
+      {
+        title: 'mens',
+        imageUrl: 'https://www3.assets-gap.com/webcontent/0011/437/954/cn11437954.jpg',
+        size: 'large',
+        id: 5,
+        linkUrl: 'shop/mens',
+      }
+    ]
+  };
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
 }
+
+export default Directory;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,40 +1,10 @@
 import React from 'react';
+import Directory from '../../components/Directory/Directory';
 import '../HomePage/HomePage.scss';
 
 const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subTitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">JACKETS</h1>
-          <span className="subTitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">SNEAKERS</h1>
-          <span className="subTitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">WOMENS</h1>
-          <span className="subTitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MENSS</h1>
-          <span className="subTitle">SHOP NOW</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 
