69114f0704fff3b36c9b35476879cc85b9332a04
coderkarma <karmadrukpa11@gmail.com>
2019-12-09 12:23:17 -0800

    updated shop data with new data and updated cart utils to group items in card

--- src/components/cart-icon/CartIcon.js
+++ src/components/cart-icon/CartIcon.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import toggleCartHidden from '../../redux/cart/cart.action';
+import { toggleCartHidden } from '../../redux/cart/cart.action';
 
 import { ReactComponent as ShoppingIcon } from '../../assests/shopping-bag.svg';
 import './CartIcon.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({ toggleCartHidden }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
     <span className='item-count'>0</span>
@@ -14,5 +14,5 @@ const CartIcon = ({toggleCartHidden}) => (
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
-})
+});
 export default connect(null, mapDispatchToProps)(CartIcon);
