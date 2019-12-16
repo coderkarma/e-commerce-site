138abe17823bb9a755cf41af87a296388f9e5da3
coderkarma <karmadrukpa11@gmail.com>
2019-12-16 14:10:33 -0800

    added style-component and modified package.json

--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
+
 import Directory from '../../components/directory/Directory';
-import './HomePage.scss';
+
+import { HomePageContainer } from './hompage.styles';
 
 const HomePage = () => (
-  <div className="homepage">
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 );
 
 export default HomePage;
